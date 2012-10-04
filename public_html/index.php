bd981184c1a9814af82e3f2146295f6d16866487
zoe slattery <zoe@php.net>
2012-10-04 13:21:12 +0100

    Addind test eventbrite

--- public_html/index.php
+++ public_html/index.php
@@ -81,7 +81,7 @@
 				</div>	
 				<div  class="sixcol last">
 				<div class="right-button">	
-					<a href="#" class="home-button yellow-button">BUY</a>
+					<a href="#" onclick ="show('buyTicket')" class="home-button yellow-button">BUY</a>
 					<div class="home-button-text">
 					<p>BLIND BIRD TICKET</p>
 					</div>
@@ -102,6 +102,12 @@
                    </article>
 
                 <article>
+                <div id="buyTicket">
+                <div style="width:100%; text-align:left;" ><iframe  src="http://www.eventbrite.com/event/4521447772?ref=eweb" frameborder="0" height="1000" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe><div style="font-family:Helvetica, Arial; font-size:10px; padding:5px 0 5px; margin:2px; width:100%; text-align:left;" ><a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com/r/eweb">Sell Tickets</a> <span style="color:#ddd;">through</span> <a style="color:#ddd; text-decoration:none;" target="_blank" href="http://www.eventbrite.com?ref=eweb">Eventbrite</a></div></div>
+                <a href="#" onclick="hide('buyTicket')" class="small-button yellow-button">CLOSE</a>
+                </div>
+                </article>
+                
                
                     <h3 class="sign-up">Sign up for more Information</h3>
                     
