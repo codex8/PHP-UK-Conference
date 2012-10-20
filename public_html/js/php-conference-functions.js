27f4cff866ecd532c1fe0f525b55b639bb0ded42
zoe slattery <zoe@php.net>
2012-10-20 22:31:45 +0100

    Move javascript to file

---
+++ public_html/js/php-conference-functions.js
@@ -0,0 +1,8 @@
+window.onload = (function(){
+    $('#header').css('height', $('.header-image').height());
+});  
+$(document).ready(function(){
+    $(window).resize(function(){
+        $('#header').css('height', $('.header-image').height());
+    });        
+});
