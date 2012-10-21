965673be7535d51cdab2e78826d61e4bc6e498c1
zoe slattery <zoe@php.net>
2012-10-21 14:47:39 +0100

    Down to 647 wide

--- public_html/js/php-conference-functions.js
+++ public_html/js/php-conference-functions.js
@@ -1,16 +1,13 @@
 window.onload = (function(){
     $('#header').css('height', $('.header-image').height());
     $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
-    $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());
-    $('#content-bar-below-header').css('overflow',  'visible');
-    
+    $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());    
 });  
 $(document).ready(function(){
     $(window).resize(function(){
         $('#header').css('height', $('.header-image').height());
         $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
         $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());
-        $('#content-bar-below-header').css('overflow',  'visible');
     });        
 });
 
