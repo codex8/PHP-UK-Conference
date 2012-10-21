68145dbcc349313702ae6d90022400814998eaed
zoe slattery <zoe@php.net>
2012-10-21 15:06:06 +0100

    Down to 450 wide - reduce bottom bar height

--- public_html/js/php-conference-functions.js
+++ public_html/js/php-conference-functions.js
@@ -1,13 +1,13 @@
 window.onload = (function(){
     $('#header').css('height', $('.header-image').height());
-    $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
-    $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());    
+    $('.ticket-image').css('height', $('#bar-below-header').height() + 0.5 * $('#bar-below-header').height());
+    $('.ticket-image').css('margin-top',  -0.25 * $('#bar-below-header').height());    
 });  
 $(document).ready(function(){
     $(window).resize(function(){
         $('#header').css('height', $('.header-image').height());
-        $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
-        $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());
+        $('.ticket-image').css('height', $('#bar-below-header').height() + 0.5 * $('#bar-below-header').height());
+        $('.ticket-image').css('margin-top',  -0.25 * $('#bar-below-header').height());
     });        
 });
 
