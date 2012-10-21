a6c16f824d6bf87dbb7f9297e846d59fafcd9821
zoe slattery <zoe@php.net>
2012-10-21 12:38:06 +0100

    Use js to place ticket image

--- public_html/js/php-conference-functions.js
+++ public_html/js/php-conference-functions.js
@@ -1,9 +1,16 @@
 window.onload = (function(){
     $('#header').css('height', $('.header-image').height());
+    $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
+    $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());
+    $('#content-bar-below-header').css('overflow',  'visible');
+    
 });  
 $(document).ready(function(){
     $(window).resize(function(){
         $('#header').css('height', $('.header-image').height());
+        $('.ticket-image').css('height', $('#content-bar-below-header').height() + 0.5 * $('#content-bar-below-header').height());
+        $('.ticket-image').css('margin-top',  -0.25 * $('#content-bar-below-header').height());
+        $('#content-bar-below-header').css('overflow',  'visible');
     });        
 });
 
