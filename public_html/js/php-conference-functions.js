99875ff7b019a2a29baa82c20f3cdab54fe7f7ac
zoe slattery <zoe@php.net>
2012-10-23 20:54:14 +0100

    Header layout fixes

--- public_html/js/php-conference-functions.js
+++ public_html/js/php-conference-functions.js
@@ -1,3 +1,4 @@
+//Header settings
 window.onload = (function(){
     $('#header').css('height', $('.header-image').height());
     $('.ticket-image').css('height', $('#bar-below-header').height() + 0.5 * $('#bar-below-header').height());
@@ -11,5 +12,11 @@ $(document).ready(function(){
     });        
 });
 
-
+//Countdown settings
+$(function () {
+	var confDay = new Date();
+	confDay = new Date(confDay.getFullYear() + 1, 3 - 1, 22);
+	$('#year').text(confDay.getFullYear());
+	$('#noSeconds').countdown({until: confDay, format: 'dHM'});
+});
 
