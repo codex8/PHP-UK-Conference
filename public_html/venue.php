8f7f544ca87aaf770d09612d16afa3d66371cc5b
Johanna Cherry <johanna@johannacherry.com>
2012-10-25 00:15:37 +0100

    Basic copy for venue page.

--- public_html/venue.php
+++ public_html/venue.php
@@ -1,15 +1,52 @@
 <?php include 'header.php'; ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" href="css/resets.css">
+
+<link rel="stylesheet" href="css/1140.css">
+<link rel="stylesheet" href="css/style.css?version=3">
+<link rel="stylesheet" href="css/media-styles.css">
+
+
+<script src="js/jquery-1.8.0.min.js"></script>
+<script src="js/css3-mediaqueries.js"></script>
+<script src="js/php-conference-functions.js"></script>
+<script src="js/jquery.countdown.js"></script>
+</head>
+<body>
+<?php include 'banner.php'; ?>
 <div id="main">
-<div id="brewery-image" class="row">
-<div class="twelvecol">
-<img class="brewery-image" src="images/brewery.jpg" />
-</div>
-</div>
-<div id="brewery-map" class="row">
-<div class="twelvecol">
-<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
-</div>
-</div>
+    <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />VENUE<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+    <!-- <div id="brewery-image" class="row">
+        <div class="twelvecol">
+            <img class="brewery-image" src="images/brewery.jpg" class="circle-frame" />
+        </div>
+    </div> -->
+    <div id="brewery-image" class="row">
+        <div class="twelvecol">
+            <div class="profile fourcol">
+                <iframe class="circle-frame" width="350" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14&amp;output=embed&amp;iwloc=near"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+            </div>
+            <div class="profile fourcol">
+            </div>
+            <div class="profile fourcol last">
+                 <p><a href="http://www.thebrewery.co.uk/">The Brewery</a> occupies the site of the former Whitbread brewery in East London. In 1750 Samuel Whitbread, having bought property in the area, transplanted his operations to Chiswell Street from two smaller breweries in Old Street and Brick Lane.</p>
+                <p>Within a few years Whitbread’s brewery had become a vast operation, its sheer size making it an attraction worthy of royal visits. George III, Queen Charlotte, Queen Elizabeth II and Elizabeth the Queen Mother all visited the site.</p>
+                <p>Brewing on the site continued until 1976, the last tanker pulling out of the South Yard on April 13, bringing to an end a 225-year era.</p>
+            </div>
+        </div>
+    </div>
+    <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />ACCOMODATION<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+    <p>There are several hotels within 5-10 minutes walking distance of the venue. We'll be working with those hotels to try and bring you a reduced booking rate and will update this page soon with more details. </p>
 </div><!-- main -->
 <div class="push"></div>
 </div><!-- page -->
