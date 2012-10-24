038525dc0be06f56b8695d22bb71684c189c8a46
zoe slattery <zoe@php.net>
2012-10-24 20:25:36 +0100

    Added map to venue page - not quite right?

--- public_html/venue.php
+++ public_html/venue.php
@@ -29,6 +29,11 @@
 <img class="brewery-image" src="images/brewery.jpg" />
 </div>
 </div>
+<div id="brewery-map" class="row">
+<div class="twelvecol">
+<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.uk/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14&amp;output=embed"></iframe><br /><small><a href="https://maps.google.co.uk/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=52+Chiswell+Street++London+EC1Y+4SD&amp;aq=&amp;sll=52.8382,-2.327815&amp;sspn=6.757115,16.45752&amp;ie=UTF8&amp;hq=&amp;hnear=52+Chiswell+St,+London+EC1Y+4SA,+United+Kingdom&amp;ll=51.520779,-0.090923&amp;spn=0.006796,0.016072&amp;t=m&amp;z=14" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+</div>
+</div>
 </div><!-- main -->
 <div class="push"></div>
 </div><!-- page -->
