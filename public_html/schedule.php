7e4873c02e364e25ff9843577465a059cc22fc05
Johanna Cherry <johanna@johannacherry.com>
2012-10-24 23:38:55 +0100

    Add some basic copy to schedule until its published.

--- public_html/schedule.php
+++ public_html/schedule.php
@@ -1,5 +1,8 @@
 <?php include 'header.php'; ?>
 <div id="main">
+    <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />SCHEDULE<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+    <p>The schedule will be published on <strong>December 6th 2012</strong>. Until then, why not submit a paper and you might be listed here in a couple weeks!</p>
+    <div id="submit-abstract"><a href="http://bit.ly/phpukcfp2013" target="_blank" class="standard-button">SUBMIT</a></div>
 </div><!-- main-->
 <div class="push"></div>
 </div><!-- page -->
