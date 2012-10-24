0c9edffc8520975e9e16805b2aabdaf9fe6cd236
zoe slattery <zoe@php.net>
2012-10-24 17:35:43 +0100

    change the footer round a bit

--- public_html/footer.php
+++ public_html/footer.php
@@ -1,22 +1,29 @@
 <div id="footer" class="footer">
 <div id= footer-content class= row>
 
-<div class="fourcol">
-<div id="php-london"> 
-<p>Powered by <a href="http://phplondon.org">PHP LONDON</a></p>
-</div>
-</div>
 
-<div class="fourcol">
+
+<div class="eightcol">
 <div id="social-media"> 
+<h3> We'd love to hear from you </h3>
+<div class="row">
+<div class="fourcol">
 <p><a href="https://twitter.com/PHPLondon" class="twitter-follow-button" data-show-count="false">Follow @PHPLondon</a>
+</div>
+<div class="fourcol">
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></p>
-<p><a href="mailto:contact@phplondon.org"><img class="email-image" src="images/email.png"/> Email us</a> </p>
+<p><a href="mailto:contact@phplondon.org"><img class="email-image" src="images/email.png"/>Send us an email</a> </p>
+</div>
+<div class="fourcol last">
+<a href="https://github.com/PHPLondon/PHP-UK-Conference/issues"><img class="octocat" src="images/octocat.png"/>Raise issues with this site</a>
+</div>
+</div>
 </div>
 </div>
 
 <div class="fourcol last">
 <div id="contact-info">
+<p>Powered by <a href="http://phplondon.org">PHP LONDON</a></p>
 <p>PHP London Limited</p>
 <p>324/326 Regent Street, London. W1B3HH</p>
 <p>Company number: 06770521</p>
