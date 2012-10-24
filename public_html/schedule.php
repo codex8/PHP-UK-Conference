2ed4c4af1a0a78114f3ae88aca6add329853a948
Johanna Cherry <johanna@johannacherry.com>
2012-10-24 22:50:37 +0100

    Adding header file for all pages to have the same assets

--- public_html/schedule.php
+++ public_html/schedule.php
@@ -1,28 +1,4 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!-->
-<html class="no-js">
-<!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta name="viewport"
-	content="width=device-width, initial-scale=1, maximum-scale=1">
-<link rel="stylesheet" href="css/resets.css">
-
-<link rel="stylesheet" href="css/1140.css">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/media-styles.css">
-
-
-<script src="js/jquery-1.8.0.min.js"></script>
-<script src="js/css3-mediaqueries.js"></script>
-<script src="js/php-conference-functions.js"></script>
-<script src="js/jquery.countdown.js"></script>
-</head>
-<body>
-<?php include 'banner.php'?>
+<?php include 'header.php'; ?>
 <div id="main">
 </div><!-- main-->
 <div class="push"></div>
