153ba4f5ef58313465aff6ebc3d10902120ce1e4
zoe slattery <zoe@php.net>
2012-10-24 10:46:11 +0100

    Add max height to header image

--- public_html/js/php-conference-functions.js
+++ public_html/js/php-conference-functions.js
@@ -1,12 +1,14 @@
 //Header settings
 window.onload = (function(){
-    $('#header').css('height', $('.header-image').height());
+	var imageHeight = $('.header-image').height();
+    $('#header').css('height', imageHeight);
     $('.ticket-image').css('height', $('#bar-below-header').height() + 0.5 * $('#bar-below-header').height());
     $('.ticket-image').css('margin-top',  -0.25 * $('#bar-below-header').height());    
 });  
 $(document).ready(function(){
     $(window).resize(function(){
-        $('#header').css('height', $('.header-image').height());
+    	var imageHeight = $('.header-image').height();
+        $('#header').css('height', imageHeight);
         $('.ticket-image').css('height', $('#bar-below-header').height() + 0.5 * $('#bar-below-header').height());
         $('.ticket-image').css('margin-top',  -0.25 * $('#bar-below-header').height());
     });        
