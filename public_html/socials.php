570a58605ffe3b01010e2039c07fc36cd94e6b54
Johanna Cherry <johanna@johannacherry.com>
2012-10-24 23:47:51 +0100

    Basic copy on the socials page.

--- public_html/socials.php
+++ public_html/socials.php
@@ -1,5 +1,20 @@
 <?php include 'header.php'; ?>
 <div id="main">
+    <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />SOCIALS<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+    <div id="profiles" class="row">
+        <div class="profile fourcol">
+            <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />THURSDAY<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+            <p>Thursday night kicks off the conference with a PHP London style pubcon. We'll meet at the pub for drinks and food at 7pm, then at 8pm we'll listen to a talk from a local community member, then a few more drinks and chat to finish of the night.</p>
+        </div>
+        <div class="profile fourcol">
+            <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />FRIDAY<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+            <p>Opening conference social, details TBD.</p>
+        </div>
+        <div class="profile fourcol">
+            <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />SATURDAY<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+            <p>Closing conference social, details TBD.</p>
+        </div>
+    </div>
 </div><!-- main-->
 <div class="push"></div>
 </div><!-- page -->
