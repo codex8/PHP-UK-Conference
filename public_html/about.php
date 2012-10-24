f04c77c623d5ab4c31ff75a8cfc85349f1d7ed46
Johanna Cherry <johanna@johannacherry.com>
2012-10-24 22:33:23 +0100

    Updating copy and profiles on the about page.

--- public_html/about.php
+++ public_html/about.php
@@ -22,31 +22,50 @@
 <script src="js/jquery.countdown.js"></script>
 </head>
 <body>
-<?php include 'banner.php'?>
+<?php include 'banner.php'; ?>
 <div id="main">
   
 <div class="row">
 <div class="twelvecol">
 <h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />ABOUT THE CONFERENCE<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
-<p>PHP London are pleased to announce the 8th Annual PHP UK conference. For the second time, the conference will run over two days with four great tracks
-In 2013 the conference will be at the Brewery, in the heart of the City of London.
-</p>
-<p>PHP UK conferences are held in London at the end of February each year. Attended by hundreds of delegates, speakers, sponsors, partners they are run
-by volunteer members of the PHP London community.
-</p>
-<p>Delegates are typically professional web developers and managers, along with some general PHP enthusiasts and employment recruiters. The 
-event comprises a mix of split-track talks, keynote presentations, panel sessions, unconference slots along with exhibition space for sponsors.
-Networking is encouraged through informal events and evening socials.
-</p>
-<p>
-Costs are minimised to keep ticket prices as low as possible. The aim is not to make a profit, but if there are any surplus funds these are 
-used to fund community events throughout the year or possibly to fund the following years conference.
-</p>
+<p><a href="http://phplondon.org">PHP London</a> are pleased to announce the 8th Annual PHP UK conference; a 2-day event with 4 great tracks held at at <a href="http://www.thebrewery.co.uk" alt="The Brewery Event Venue" title="The Brewery Event Venue">The Brewery</a>, in the heart of the City of London.</p>
+
+<p>With over 600 delegates, speakers, and sponsors, PHP UK conference aims to deliver fantastic up to date content about PHP and related web technologies in a comfortable and professional setting. There are countless networking opportunities to engage with international speakers and delegates, which makes the event one you won't want to miss.</p>
+
+<p>Anyone is welcome to attend the event, whether you're a professional web developer, hobbyist, manager, or just starting out, there's something for everyone.</p>
+
+<p>The event kicks off on Thursday evening with a special pubcon sponsored by PHP London. Each day will then start with an inspiring keynote presentation, followed by your choice of 3 tracks of talks or unconference sessions. Each day will close with a panel session followed by the evening's social event. There's also exhibition space for all sponsors, to fill in the spaces in between.</p>
+
+
+<p>PHP UK Conference is run by volunteer members of the PHP London community. Costs are minimised to keep ticket prices as low as possible. The aim is not to make a profit, but if there are any surplus funds these are used to fund community events throughout the year and to fund the following years' conference.</p>
+
+<h1><img class="left-ribbon" src="images/TitleRibbonLeft.png" />ORGANISERS<img class="right-ribbon" src="images/TitleRibbonRight.png"/></h1>
+<div class="profile">
+    <img class="circle-frame" src="http://www.gravatar.com/avatar/1534835c0e1afc0772f7017c5528ddf4" />
+    <p><a href="http://zoah.co.uk">Zo&euml;</a> has worked for over 30 years in the IT industry, and during that time she has held a variety of roles ranging from the deeply technical to senior management. As a technology professional she has worked on storage products (disk and tape firmware), high performance computing (FORTRAN) and is currently a committer for both the PHP language and Apache Aries. As a senior manager, she was responsible for leading teams of up to forty engineers.</p>
+    <p>Zo&euml; is currently a Director of PHP London and manages the web site, registration, volunteers and exhibits for the conference.</p>
+    <p><a class="btn" href="http://twitter.com/zoe_slattery">@zoe_slattery on Twitter &raquo;</a></p>
+    <p><a class="btn" href="http://github.com/zoes">@zoes on Github &raquo;</a></p>
+</div>
+<div class="profile">
+    <img class="circle-frame" src="http://www.gravatar.com/avatar/a5bb97e83780c26b2a8862fedc4a81d2" />
+    <p><a href="http://uk.linkedin.com/in/ciaranr" target="_blank">Ciar&aacute;n</a> is the <a href="http://skimlinks.com/team#ciaran" target="_blank">CTO of Skimlinks</a> where he leads the technical innovation for Skimlinks' products and platform's infrastructure. Before Skimlinks, Ciar&aacute;n worked in Ireland as Head of IT at one of the country's largest <a href="http://realm.ie/" target="_blank">mobile communications companies</a> and established <a href="http://www.crunchbase.com/company/weeno-media" target="_blank">Weeno Ltd</a>. Ciaran has been developing PHP applications at large scale for the past 10 years since he first studied <a href="http://www.dcu.ie/prospective/deginfo.php?classname=DME&amp;originating_school=20" target="_blank">Digital Media Engineering</a> in Dublin City University and holds a Honors Diploma in Multimedia from <a href="http://www.uwl.ac.uk/" target="_blank">University of West London</a>.</p>
+    <p>Ciar&aacute;n is currently a Director of PHP London and manages sponsors, socials and marketing for the conference.</p>
+    <p><a class="btn" href="http://twitter.com/CiaranR" target="_blank">@CiaranR on Twitter &raquo;</a></p>
+    <p><a class="btn" href="http://github.com/CiaranR">@CiaranR on Github &raquo;</a></p>
+</div>
+<div class="profile">
+    <img class="circle-frame" src="http://www.gravatar.com/avatar/cfba4698b0acef6927797ff274ba5921" />
+    <p><a href="http://johannacherry.com">Johanna Cherry</a> is a software engineer at <a href="http://hailocab.com">Hailo</a> in London working on their mobile APIs and services. She has over 10 years experience developing web applications with PHP and lots of other cool web technologies. Prior to Hailo, she was Development Manager for the affiliate network <a href="http://webgains.com">Webgains</a> and Technical Team Lead for the PHP consultancy <a href="http://ibuildings.com">Ibuildings</a>. She earned a Bachelor of Science in Technology Management from <a href="http://herzing.edu">Herzing University</a> in Madison Wisconsin.</p>
+    <p>Johanna is currently a Director of PHP London and manages speakers, the venue, and delegate services for the conference.</p>
+    <p><a class="btn" href="http://twitter.com/johannacherry">@johannacherry on Twitter &raquo;</a></p>
+    <p><a class="btn" href="http://github.com/johannac">@johannac on Github &raquo;</a></p>
+</div>
 </div>
 </div>
 </div><!-- main-->
 <div class="push"></div>
 </div><!-- page -->
-<?php include 'footer.php'?>
+<?php include 'footer.php'; ?>
 </body>
 </html>
