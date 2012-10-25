fcb86e06333a8c8741ea315226a471d4b3c3e8fc
zoe slattery <zoe@php.net>
2012-10-25 10:52:22 +0100

    Do we have the wrong location for 404? Test alternative

---
+++ public_html/addons/error/404.php
@@ -0,0 +1,16 @@
+<?php 
+$host = $_SERVER['HTTP_HOST'];
+$menu="home";
+include '../../public_html/header-tpl.php';
+?>
+<div id="main">
+<div id="FourOhFour" class="row">
+We have not been able to find the page you are looking for and it has made the Elephant sad.
+<img src="images/SadElephant.png" class="sadele">
+</div>
+</div><!-- main-->
+<div class="push"></div>
+</div><!-- page -->
+<?php include '../../public_html/footer-tpl.php'?>
+</body>
+</html>
