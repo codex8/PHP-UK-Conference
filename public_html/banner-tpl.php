83b24bd5fb69eee7672bb106306cc7a287841d16
CiaranR <ciaran.rooney@gmail.com>
2012-10-25 04:04:27 +0100

    more absolute paths

--- public_html/banner-tpl.php
+++ public_html/banner-tpl.php
@@ -6,29 +6,29 @@
 
 <div id="page">
 <div id="header"><img class="header-image"
-	src="images/HeaderBackground.png" alt="" />
+	src="//<?=$host;?>/images/HeaderBackground.png" alt="" />
 <div id="access" class="row">
 <div id="left-menu">
 <ul>
-	<li><a href="/about">ABOUT</a></li>
-	<li><a href="/speakers">SPEAKERS</a></li>
-	<li><a href="/schedule">SCHEDULE</a></li>
+	<li><a href="//<?=$host;?>/about">ABOUT</a></li>
+	<li><a href="//<?=$host;?>/speakers">SPEAKERS</a></li>
+	<li><a href="//<?=$host;?>/schedule">SCHEDULE</a></li>
 </ul>
 </div>
 
 <div id="right-menu">
 <ul>
-	<li><a href="/sponsors">SPONSORS</a></li>
-	<li><a href="/socials">SOCIALS</a></li>
-	<li><a href="/venue">VENUE</a></li>
+	<li><a href="//<?=$host;?>/sponsors">SPONSORS</a></li>
+	<li><a href="//<?=$host;?>/socials">SOCIALS</a></li>
+	<li><a href="//<?=$host;?>/venue">VENUE</a></li>
 </ul>
 </div>
 </div>
-<div id="header-logo"><a href="/"><img class="header-logo"
-	src="images/HeaderHead.png" alt="" /></a></div>
+<div id="header-logo"><a href="//<?=$host;?>/"><img class="header-logo"
+	src="//<?=$host;?>/images/HeaderHead.png" alt="" /></a></div>
 
 <div id="header-text"><span class="small-title-text">F&nbsp;E&nbsp;B&nbsp;R&nbsp;U&nbsp;A&nbsp;R&nbsp;Y&nbsp;&nbsp;&nbsp;22&nbsp;-&nbsp;23&nbsp;&nbsp;&nbsp;2013&nbsp;&nbsp;&nbsp;&#8212;&nbsp;&nbsp;&nbsp;</span>
-<a  href="/"><span class="big-title-text">PHP UK</span></a> <span
+<a  href="//<?=$host;?>/"><span class="big-title-text">PHP UK</span></a> <span
 	class="small-title-text">&nbsp;&#8212;&nbsp;&nbsp;T&nbsp;H&nbsp;E&nbsp;&nbsp;B&nbsp;R&nbsp;E&nbsp;W&nbsp;E&nbsp;R&nbsp;Y,&nbsp;&nbsp;L&nbsp;O&nbsp;N&nbsp;D&nbsp;O&nbsp;N</span>
 </div>
 <div id="header-sub-text"><span class="sub-title-text">
@@ -36,10 +36,10 @@ CONFERENCE 2013 </span></div>
 
 <div id="header-circle-wrapper" class="row">
 <div id="header-circles" class="twelvecol"><img class="circle"
-	src="images/Round1.png" alt="" /> <img class="circle"
-	src="images/Round2.png" alt="" /> <img class="circle"
-	src="images/Round3.png" alt="" /> <img class="circle"
-	src="images/Round4.png" alt="" /></div>
+	src="//<?=$host;?>/images/Round1.png" alt="" /> <img class="circle"
+	src="//<?=$host;?>/images/Round2.png" alt="" /> <img class="circle"
+	src="//<?=$host;?>/images/Round3.png" alt="" /> <img class="circle"
+	src="//<?=$host;?>/images/Round4.png" alt="" /></div>
 </div>
 </div>
 <div id="bar-below-header" class="transparent"></div>
@@ -52,9 +52,9 @@ UNTIL THE PHP UK CONFERENCE
 </div>
 <div class="fourcol last"><a
 	href="http://phpukconference.eventbrite.com/"><img
-	class="ticket-image alignright" src="images/Ticket.png" alt="" /></a></div>
+	class="ticket-image alignright" src="//<?=$host;?>/images/Ticket.png" alt="Buy Tickets Now" /></a></div>
 </div>
 <div id="mobile-content-bar-below-header" class="row"><a
 	href="http://phpukconference.eventbrite.com/"><img
-	class="ticket-image" src="images/Ticket.png" alt="" /></a>
+	class="ticket-image" src="//<?=$host;?>/images/Ticket.png" alt="Buy Tickets Now" /></a>
 </div>
