4447f5f3dd80624090d43d1be295abef3e482967
zoe slattery <zoe@php.net>
2012-10-25 12:07:14 +0100

    Style 404 - more

--- public_html/addons/error/404.php
+++ public_html/addons/error/404.php
@@ -8,7 +8,9 @@ include '../../public_html/header-tpl.php';
 We have not been able to find the page you are looking for and it has made the Elephant sad.
 </div>
 <div class="row">
+<div id ="elephant-image" class= "twelvecol">
 <img src="images/SadElephant.png" class="sadele">
+</div>
 </div>
 </div><!-- main-->
 <div class="push"></div>
