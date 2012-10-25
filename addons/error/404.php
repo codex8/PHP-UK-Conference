0640ffaa1387ad63e30d670f573ba1163e362155
zoe slattery <zoe@php.net>
2012-10-25 11:56:24 +0100

    Style 404

--- addons/error/404.php
+++ addons/error/404.php
@@ -6,6 +6,8 @@ include '../../public_html/header-tpl.php';
 <div id="main">
 <div id="FourOhFour" class="row">
 We have not been able to find the page you are looking for and it has made the Elephant sad.
+</div>
+<div class="row">
 <img src="images/SadElephant.png" class="sadele">
 </div>
 </div><!-- main-->
