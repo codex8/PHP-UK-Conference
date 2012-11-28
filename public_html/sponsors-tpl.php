8cfff47b2bb34264513cdc4efab14cf144966a2e
zoe slattery <zoe@php.net>
2012-11-28 11:23:23 +0000

    Minor wording change

--- public_html/sponsors-tpl.php
+++ public_html/sponsors-tpl.php
@@ -1,6 +1,6 @@
 <div id="main">
     <h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" />SPONSORS<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png"/></h1>
-    <p>PHP UK is a not-for-profit event and relies heavily on sponsorship to run the event at rates affordable to the whole community. If you're interested in sponsoring our event, please <a href="mailto:contact@phpconference.co.uk">contact us</a>.</p>
+    <p>The PHP UK Conference is a not-for-profit event and relies heavily on sponsorship to run the event at rates affordable to the whole community. If you're interested in sponsoring our event, please <a href="mailto:contact@phpconference.co.uk">contact us</a>.</p>
 </div><!-- main -->
 <div class="push"></div>
 </div><!-- page -->
