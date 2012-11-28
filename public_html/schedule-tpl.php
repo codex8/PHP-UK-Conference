23c9ae4d77c8526b875fe5cc3e31c28e891c2267
zoe slattery <zoe@php.net>
2012-11-28 10:47:53 +0000

    Take link to submission of schedule page

--- public_html/schedule-tpl.php
+++ public_html/schedule-tpl.php
@@ -1,7 +1,6 @@
 <div id="main">
     <h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" />SCHEDULE<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png"/></h1>
-    <p>The schedule will be published on <strong>December 6th 2012</strong>. Until then, why not <a href="/speakers.php">submit a paper</a> and you might be listed here in a couple weeks!</p>
-    <div id="submit-abstract"><a href="http://bit.ly/phpukcfp2013" target="_blank" class="standard-button">SUBMIT</a></div>
+    <p>The schedule will be published on <strong>December 6th 2012</strong>.</p>
 </div><!-- main-->
 <div class="push"></div>
 </div><!-- page -->
