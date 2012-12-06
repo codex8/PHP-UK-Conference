90b9c38407f662baadb0ef5c92664e32f73969b4
zoe slattery <zoe@php.net>
2012-12-06 09:05:53 +0000

    Front page changes

--- public_html/home-tpl.php
+++ public_html/home-tpl.php
@@ -1,7 +1,7 @@
 <div id="main">
 
 <div class="row">
-<div class="fourcol">
+<div class="sixcol">
 <h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" alt=""/>SAVE
 THE DATE<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png" /></h1>
 <p>PHP London are pleased to announce our 8th Annual PHP UK
@@ -9,16 +9,11 @@ conference. It will be two days crammed with great talks and amazing
 socials. This year the conference will be in the Brewery in the heart of
 the City of London.</p>
 </div>
-<div class="fourcol">
-<h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" alt=""/>SUBMIT
-AN ABSTRACT<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png" /></h1>
-<p>Our call for papers is now closed, thanks to all of you who submitted!
-</p>
-</div>
-<div class="fourcol last">
+<div class="sixcol last">
 <h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" />BUY
 YOUR TICKETS<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png" /></h1>
-<p>Blind bird tickets are on sale from now until the 6th December.
+<p>Early bird tickets ar on sale from 6th December.
+Check our fantastic <a href="/speakers">speakers</a> and <a href="/schedule">schecdule!</a>
 Click on the ticket image above to buy now and beat the crowd!</p>
 </div>
 </div>
