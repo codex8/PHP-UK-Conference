2a1d013008a3122ba4e8ed2fd0b613265198d77c
zoe slattery <zoe@php.net>
2013-01-13 15:48:45 +0000

    sponsor logos at bottom of page

--- public_html/home-tpl.php
+++ public_html/home-tpl.php
@@ -1,7 +1,7 @@
 <div id="main">
 
 <div class="row">
-<div class="sevencol">
+<div class="twelvecol">
 <h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" />THE CONFERENCE<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png"/></h1>
 <p>PHP London are pleased to announce our 8th Annual PHP UK
 conference. It will be two days crammed with great talks and amazing
@@ -9,18 +9,6 @@ socials. This year the conference will be in the Brewery in the heart of
 the City of London.</p>
 <p>Check out our fantastic <a href="/speakers">speakers</a> and <a href="/schedule">schedule!</a></p>
 <p>Click on the ticket image above to buy now and beat the crowd!</p>
-</div>
-<div class="fivecol last">
-<h1><img class="left-ribbon" src="//<?=$host;?>/images/TitleRibbonLeft.png" />OUR SPONSORS<img class="right-ribbon" src="//<?=$host;?>/images/TitleRibbonRight.png"/></h1>
-</div> 
-<?php 
-require_once('functions.php');
-$logos = array ("INVIQA" => "http://inviqa.com", 
-                "SENSIO" => "http://sensiolabs.co.uk" ,
-                "WEBANDPHP" => "www.webandphp.com");
-display_logos($logos);
-?>
-
 </div>
 </div>
 <div id="sponsor-logos-bar">
